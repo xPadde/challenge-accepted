4050dc7a7ae6abf2d5d5ae54a9f0410d4fa0b48e
Thomas Lansing <thompa.lansing@gmail.com>
2016-04-27 21:39:21 +0200

    add the test folder and the first test class

---
+++ src/test/com/challengeaccepted/controllers/ChallengeControllerTest.java
@@ -0,0 +1,50 @@
+package com.challengeaccepted.controllers;
+
+import com.challengeaccepted.models.ChallengeModel;
+import com.challengeaccepted.services.ChallengeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+public class ChallengeControllerTest {
+
+    @Mock
+    private ChallengeService challengeService;
+
+    @InjectMocks
+    private ChallengeController challengeController;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testCreateChallenge() throws Exception {
+        assertEquals("createChallenge() did not respond with http status 201 (created)", new ResponseEntity(HttpStatus.CREATED), challengeController.createChallenge(new ChallengeModel()));
+    }
+
+    @Test
+    public void testReadChallenge() throws Exception {
+        assertEquals("readChallenge() did not respond with http status 200 (ok)", new ResponseEntity(HttpStatus.OK), challengeController.readChallenge(new Random().nextLong()));
+    }
+
+    @Test
+    public void testUpdateChallenge() throws Exception {
+        assertEquals("updateChallenge() did not respond with http status 200 (ok)", new ResponseEntity(HttpStatus.OK), challengeController.updateChallenge(new Random().nextLong()));
+    }
+
+    @Test
+    public void testDeleteChallenge() throws Exception {
+        assertEquals("deleteChallenge() did not respond with http status 200 (ok)", new ResponseEntity(HttpStatus.OK), challengeController.deleteChallenge(new Random().nextLong()));
+    }
+
+}
