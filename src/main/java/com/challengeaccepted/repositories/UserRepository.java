90de422ef8bd474a16b48f5df307e650b83dede5
joel nilsson <joel nilsson>
2016-04-29 09:59:46 +0200

    fix usermodel and CR to that

---
+++ src/main/java/com/challengeaccepted/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.challengeaccepted.repositories;
+
+import com.challengeaccepted.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by joel_ on 2016-04-29.
+ */
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+
+}
