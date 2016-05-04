5edf81dff70220956be1f492b8d50e21728a2c60
Thomas Lansing <thompa.lansing@gmail.com>
2016-05-04 15:20:20 +0200

    Add function to add YouTube-video

--- src/main/resources/app/controllers/challenge-controller.js
+++ src/main/resources/app/controllers/challenge-controller.js
@@ -1,4 +1,4 @@
-app.controller('ChallengeController', ['$scope', '$http', 'challengeService', 'userService', function ($scope, $http, challengeService, userService) {
+app.controller('ChallengeController', ['$scope', '$http', '$sce', 'challengeService', 'userService', function ($scope, $http, $sce, challengeService, userService) {
 
     var list = [];
     $scope.orderByField = 'creationDate';
@@ -31,7 +31,7 @@ app.controller('ChallengeController', ['$scope', '$http', 'challengeService', 'u
             .success(function () {
                 console.log('challengeService.createNewChallenge() called and it created a new challenge and saved it to the database!');
                 $scope.getListOfChallenges()
-                
+
             })
             .error(function () {
                 console.log('challengeService.createNewChallenge() called and it ***FAILED*** to create new challenge');
@@ -51,36 +51,36 @@ app.controller('ChallengeController', ['$scope', '$http', 'challengeService', 'u
 
     $scope.upvoteChallenge = function (challenge) {
         challenge.upvotes += 1;
-/*        challengeService.updateChallenge(challenge);*/
+        /*        challengeService.updateChallenge(challenge);*/
         loggedInUser = angular.fromJson(sessionStorage.getItem("loggedInUser"));
         challenge.challengeUpvoters.push(loggedInUser);
-        challengeService.updateChallenge(angular.toJson(challenge)).success(function(){
-          console.log("add upvote: nu gick det bra");
-        }).error(function(){
+        challengeService.updateChallenge(angular.toJson(challenge)).success(function () {
+            console.log("add upvote: nu gick det bra");
+        }).error(function () {
             console.log("add upvote: nu gick det INTE bra");
         });
-        
+
         /*challengeService.addChallengeToUpvotedChallenges(angular.toJson(challenge)).success(function(){
-           console.log("add upvote: nu gick det bra");
-        }).error(function(){
-            console.log("add upvote: nu gick det INTE bra");
-        });*/
-    }
+         console.log("add upvote: nu gick det bra");
+         }).error(function(){
+         console.log("add upvote: nu gick det INTE bra");
+         });*/
+    };
 
     $scope.claimCurrentChallenge = function (challenge) {
-        
+
         loggedInUser = angular.fromJson(sessionStorage.getItem("loggedInUser"));
-        
+
         challenge.challengeClaimer = loggedInUser;
         challenge.challengeClaimed = true;
-        
-        challengeService.updateChallenge(angular.toJson(challenge)).success(function(){
+
+        challengeService.updateChallenge(angular.toJson(challenge)).success(function () {
             console.log("claim challenge: Gick bra");
-        }).error(function(data){
+        }).error(function (data) {
             console.log("claim challenge: gick INTE bra");
             console.log(data);
         });
-        
+
     };
 
     // Update the list of challenges on page load.
@@ -92,5 +92,30 @@ app.controller('ChallengeController', ['$scope', '$http', 'challengeService', 'u
         console.log("challengeProfileToView object: ");
         console.log($scope.challengeProfileToView);
     };
-    
+
+    $scope.addYoutubeUrlToCurrentChallenge = function (challenge) {
+        var userProvidedUrl = $('#input-youtube-url').val();
+        challenge.youtubeURL = convertToYouTubeEmbedUrl(userProvidedUrl);
+    };
+
+
+    $scope.markUrlAsTrusted = function (src) {
+        return $sce.trustAsResourceUrl(src);
+    };
+
+    var convertToYouTubeEmbedUrl = function (url) {
+        var isYouTubeUrlCorrect = url.indexOf("watch?v=") > 1;
+        var baseUrl = "https://www.youtube.com/embed/";
+
+        if (isYouTubeUrlCorrect) {
+            var youTubeVideoId = url.substr(32);
+            var finalUrl = baseUrl + youTubeVideoId;
+        } else {
+            alert("Please provide a valid YouTube URL");
+            return "";
+        }
+        return finalUrl;
+    }
+
+
 }]);
