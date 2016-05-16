d1fd12a45cf4f93f7d4d60fd6784f064d57f4099
Thomas Lansing <thompa.lansing@gmail.com>
2016-05-16 14:37:38 +0200

    Add updated sql data

--- sql-data/challengeaccepted_users.sql
+++ sql-data/challengeaccepted_users.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
 --
--- Host: 127.0.0.1    Database: challengeaccepted
+-- Host: localhost    Database: challengeaccepted
 -- ------------------------------------------------------
--- Server version	5.7.12-log
+-- Server version	5.7.11
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,30 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `challenges_users`
+-- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `challenges_users`;
+DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `challenges_users` (
-  `upvotedChallenges_id` bigint(20) NOT NULL,
-  `challengeUpvoters_id` bigint(20) NOT NULL,
-  KEY `FK_5r2dk4l5ml448exgp5x4q6adk` (`challengeUpvoters_id`),
-  KEY `FK_1oeamh7hqk9h2ct6orha6tic1` (`upvotedChallenges_id`),
-  CONSTRAINT `FK_1oeamh7hqk9h2ct6orha6tic1` FOREIGN KEY (`upvotedChallenges_id`) REFERENCES `challenges` (`id`),
-  CONSTRAINT `FK_5r2dk4l5ml448exgp5x4q6adk` FOREIGN KEY (`challengeUpvoters_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `completedChallengePoints` bigint(20) DEFAULT NULL,
+  `createdChallengePoints` bigint(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `totalChallengePoints` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `challenges_users`
+-- Dumping data for table `users`
 --
 
-LOCK TABLES `challenges_users` WRITE;
-/*!40000 ALTER TABLE `challenges_users` DISABLE KEYS */;
-INSERT INTO `challenges_users` VALUES (1,1),(1,2),(2,1),(2,2),(3,1),(4,1);
-/*!40000 ALTER TABLE `challenges_users` ENABLE KEYS */;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,NULL,NULL,'thompa.lansing@gmail.com','Thomas','Lansing',NULL),(2,NULL,NULL,'kallebrallsson1@gmail.com','Kalle','Brallsson',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -51,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-05-11 16:35:25
+-- Dump completed on 2016-05-16 14:36:32
