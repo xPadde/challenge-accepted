2692eedcb6e692aad5eee680c34a8d0275530786
xPadde <viruz33@hotmail.com>
2016-05-19 18:34:48 +0200

    test on ngRoute Provider

--- src/main/resources/app/app.js
+++ src/main/resources/app/app.js
@@ -1,12 +1,49 @@
-var app = angular.module('ChallengeAccepted', ['ngValidate']);
+var app = angular.module('ChallengeAccepted', ['ngValidate', 'ngRoute']);
 
 // Requires 'ngRoute' dependency.
-/*
-app.config(function ($routeProvider){
 
-    $routeProvider.when( '/', {
-        templateUrl: 'index.html',
-        controller: 'ChallengeController'
-    })
+app.config(function ($routeProvider) {
+
+    $routeProvider
+        .when('/available-challenges', {
+            title: 'List of available challenges',
+            templateUrl: 'views/list-challenges.html',
+            controller: 'ChallengeController'
+        })
+        .when('/completed-challenges', {
+            title: 'List of completed challenges',
+            templateUrl: 'views/list-completed-challenges.html',
+            controller: 'ChallengeController'
+        })
+        .when('/create-challenge', {
+            title: 'Create a new challenge',
+            templateUrl: 'views/create-challenge.html',
+            controller: 'ChallengeController'
+        })
+        .when('/claimed-challenges', {
+            title: 'Your claimed challenges',
+            templateUrl: 'views/list-claimed-challenges.html',
+            controller: 'ChallengeController'
+        })
+        .when('/approve-video', {
+            title: 'Approve video',
+            templateUrl: 'views/list-approve-videos.html',
+            controller: 'ChallengeController'
+        })
+        .when('/home', {
+            title: 'Challenge accepted',
+            templateUrl: '/index.html',
+            controller: 'ChallengeController'
+        })
+        .when('/notifications', {
+            title: 'Notifications',
+            templateUrl: 'views/notifications.html',
+            controller: 'ChallengeController'
+        })
+        .when('/toplist', {
+            title: 'Toplist',
+            templateUrl: 'views/list-users.html',
+            controller: 'ChallengeController'
+        })
+        .otherwise({redirectTo: "/home"})
 });
-*/
