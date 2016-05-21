2a82cbeea20aea8ca423aad9249b53a2470b0f54
Patrik Andersson <patrik_andersson1985@hotmail.com>
2016-05-21 10:50:42 +0200

    removed logs

--- src/main/resources/app/controllers/notifications-controller.js
+++ src/main/resources/app/controllers/notifications-controller.js
@@ -1,6 +1,5 @@
 app.controller('NotificationsController', ['scopeService', 'notificationService', '$scope',
     function (scopeService, notificationService, $scope) {
-        console.log("Går in i notifications-controller");
 
         $scope.loggedInUser = scopeService.getLoggedInUser();
 
@@ -16,11 +15,9 @@ app.controller('NotificationsController', ['scopeService', 'notificationService'
          Below code handles notifications.
          */
         $scope.updateListOfNotifications = function () {
-            console.log("går in i updateListOf");
             notificationService.getAllNotifications($scope.loggedInUser.id)
                 .success(function (response) {
                     console.log('notificationService.getAllNotifications() was successfully executed!');
-                    console.log(response);
                     $scope.listOfNotifications = response;
                 })
                 .error(function (error) {
