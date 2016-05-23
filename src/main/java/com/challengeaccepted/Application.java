94c8590bb32ef4183fb71c0972545358d4b232ba
joel nilsson <joel nilsson>
2016-05-23 16:38:15 +0200

    fix application.class

--- src/main/java/com/challengeaccepted/Application.java
+++ src/main/java/com/challengeaccepted/Application.java
@@ -3,9 +3,11 @@ package com.challengeaccepted;
 import org.apache.juli.FileHandler;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
