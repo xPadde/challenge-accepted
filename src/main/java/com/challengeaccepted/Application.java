649fb9c78fbb822918d911e0395dcb16cd718a4c
Thomas Lansing <thompa.lansing@gmail.com>
2016-05-24 09:24:58 +0200

    Deploy Test

--- src/main/java/com/challengeaccepted/Application.java
+++ src/main/java/com/challengeaccepted/Application.java
@@ -1,17 +1,17 @@
 package com.challengeaccepted;
 
-import org.apache.juli.FileHandler;
-import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
-
         SpringApplication.run(Application.class, args);
-
     }
+
 }
