eafc28e6e9c017d8e3107eea468fd736de556a5f
xPadde <viruz33@hotmail.com>
2016-05-24 18:16:47 +0200

    fix list no refactoring

--- src/main/resources/public/app/controllers/user-controller.js
+++ src/main/resources/public/app/controllers/user-controller.js
@@ -1,5 +1,5 @@
-app.controller('UserController', ['$scope', '$location', '$routeParams', 'userService', 'scopeService',
-    function ($scope, $location, $routeParams, userService, scopeService) {
+app.controller('UserController', ['$scope', '$location', '$log', '$routeParams', 'userService',
+    function ($scope, $location, $log, $routeParams, userService) {
 
         var userUrlId = $routeParams.id;
         userService.getUserById(userUrlId)
@@ -9,5 +9,17 @@ app.controller('UserController', ['$scope', '$location', '$routeParams', 'userSe
             .error(function () {
                 $location.path('/error-user');
             });
+
+        $scope.getListOfCompletedChallenges = function () {
+            challengeService.getListOfCompletedChallenges()
+                .success(function (response) {
+                    $log.info("challengeService.getListOfCompletedChallenges() fetched all the completed challenges from the database successfully!");
+                    $scope.listOfCompletedChallenges = response;
+                })
+                .error(function (error) {
+                    $log.error("challengeService.getListOfCompletedChallenges() ***FAILED*** to fetch the completed challenges from the database!");
+                    $log.error(error);
+                });
+        };
 
     }]);
