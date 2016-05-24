0abf9d93e5df99b606ae633a01c7956397e91cae
joel nilsson <joel nilsson>
2016-05-24 11:59:39 +0200

    fix application

--- src/main/java/com/challengeaccepted/Application.java
+++ src/main/java/com/challengeaccepted/Application.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
-@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
