76ad94e0523597c079d400867c7ba105976bb910
Thomas Lansing <thompa.lansing@gmail.com>
2016-11-15 15:47:23 +0100

    Develop Spring Security with more functions following a tutorial

--- src/main/java/com/challengeaccepted/security/SecurityConfig.java
+++ src/main/java/com/challengeaccepted/security/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.challengeaccepted.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -13,6 +17,19 @@ public class SecurityConfig {
                 .withUser("user")
                 .password("password")
                 .roles("USER");
+    }
+
+    // Tutorial: http://docs.spring.io/spring-security/site/docs/3.2.0.RC2/reference/htmlsingle/#authorize-requests
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                    .antMatchers("/api/**").denyAll()
+                    .anyRequest()
+                    .authenticated()
+                .and()
+                    .formLogin()
+                .and()
+                    .httpBasic();
     }
 
 }
