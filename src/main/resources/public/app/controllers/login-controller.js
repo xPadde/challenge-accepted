76e248cb4ba5231152dac74f242918a65a48f89e
Lena <lena.stridsberg@gmail.com>
2016-11-24 14:12:27 +0100

    Add javadoc for login-controller

--- src/main/resources/public/app/controllers/login-controller.js
+++ src/main/resources/public/app/controllers/login-controller.js
@@ -5,12 +5,12 @@ app.controller('LoginController', ['$scope', '$route', '$log', '$location', 'use
         $scope.mailtoSubject = "You%20have%20been%20Challenged!";
         $scope.mailtoBody = "A%20friend%20of%20yours%20challenge%20you.%20Do%20you%20accept?%0AGo%20to:%20" + $scope.absoluteUrl;
 
-        /*
-         Below Code handles the user login.
+        /**
+         * Below Code handles the user login.
          */
 
         /**
-         * This method collects information from the login method that demands a YubiKey-password.
+         * Collects information from the login method that demands a YubiKey-password.
          */
         $scope.getLoginInfo = function () {
             return JSON.stringify({
@@ -20,7 +20,19 @@ app.controller('LoginController', ['$scope', '$route', '$log', '$location', 'use
             });
         };
 
-        $scope.login = function() {;
+        /**
+         * @param profile is a Google-user.
+         * Collects information from Google login.
+         */
+        $scope.getUserInfo = function (profile) {
+            return JSON.stringify({
+                'firstName': profile.getGivenName(),
+                'lastName': profile.getFamilyName(),
+                'email': profile.getEmail()
+            });
+        };
+
+        $scope.login = function() {
             var user = JSON.parse($scope.getLoginInfo());
 
            userService.validateLocalLogin(user)
@@ -31,14 +43,6 @@ app.controller('LoginController', ['$scope', '$route', '$log', '$location', 'use
                .error(function(response) {
                    $log.error('***FAILED*** to login!');
                })
-        };
-
-        $scope.getUserInfo = function (profile) {
-            return JSON.stringify({
-                'firstName': profile.getGivenName(),
-                'lastName': profile.getFamilyName(),
-                'email': profile.getEmail()
-            });
         };
 
         $scope.setLoggedInUser = function (response) {
@@ -91,9 +95,9 @@ app.controller('LoginController', ['$scope', '$route', '$log', '$location', 'use
             scopeService.viewUserProfilePage(user);
         };
 
-        /*
-         Below code handles the toggling of the different sections in index.html.
-         The methods is assigned to the ng-clicks.
+        /**
+         * Below code handles the toggling of the different sections in index.html.
+         * The methods is assigned to the ng-clicks.
          */
         $scope.showCreateChallenge = function () {
             if (sessionStorage.getItem("isLoggedIn") == 'true') {
